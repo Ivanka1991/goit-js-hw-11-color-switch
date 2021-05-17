d89c82c46a861a9de826c9233ce033fa9af19781
Ivanka1991 <73844703+Ivanka1991@users.noreply.github.com>
2021-05-17 22:49:39 +0300

    написала код

--- color-switch.js
+++ color-switch.js
@@ -14,6 +14,38 @@ const colors = [
     '#009688',
     '#795548',
   ];
+const refs={
+   body: document.querySelector("body"),
+   startBtn: document.querySelector('button[data-action="start"]'),
+   stopBtn: document.querySelector('button[data-action="stop"]'),
+  };
+
   const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
+ 
+const timer = {
+isActive: false,
+start(){
+  if(this.isActive){
+    return; 
+  }
+this.isActive = true;
+this.switchColor = setInterval(()=>{
+  const min = 0;
+  const max = colors.length - 1;
+  let i = randomIntegerFromInterval(min, max);
+  refs.body.style.backgroundColor = colors[i];
+}, 500);
+},
+stop() {
+clearInterval(this.switchColors);
+this.isActive = false;
+},
+};
+
+refs.startBtn.addEventListener('click', timer.start.bind(timer));
+refs.stopBtn.addEventListener('click', timer.stop.bind(timer));
+
+
+ 
